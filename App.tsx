9e0290c4667a11b6d1d7e45b6c0e5ebe357324c7
JonathanG321 <JonathanG321@ymail.com>
2023-10-25 14:43:42 -0400

    add basic app routing

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import Auth from './components/Auth';
 import Account from './components/Account';
 import { View } from 'react-native';
 import { Session } from '@supabase/supabase-js';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -19,5 +21,17 @@ export default function App() {
     });
   }, []);
 
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" children={() => Home({ session })} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Home({ session }: { session: Session | null }) {
   return <View>{session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}</View>;
 }
