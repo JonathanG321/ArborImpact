d94bd97b722ac5ca5d0fa0e78cd6d6b585c8ebac
JonathanG321 <JonathanG321@ymail.com>
2023-10-27 20:53:12 -0400

    improve LoadingScreen to show on top of everything

--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import { Text } from 'react-native-elements';
 
 export default function LoadingScreen() {
   return (
-    <View className="flex items-center justify-center h-full w-full">
+    <View className="absolute z-10 top-0 left-0 right-0 bottom-0 flex items-center justify-center h-full w-full bg-white">
       <Text className="text-3xl font-bold">Loading...</Text>
     </View>
   );
