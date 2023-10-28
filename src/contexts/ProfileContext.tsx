ac7dd467d97194fbed5de99c72c28c6c399eecbc
JonathanG321 <JonathanG321@ymail.com>
2023-10-27 20:53:49 -0400

    add profile context

---
+++ src/contexts/ProfileContext.tsx
@@ -0,0 +1,38 @@
+import { Session } from '@supabase/supabase-js';
+import { createContext, PropsWithChildren, useContext, useEffect, useState } from 'react';
+import { supabase } from '../../supabase/supabase';
+import { LoadingContext } from './LoadingContext';
+import { Alert } from 'react-native';
+import { SessionContext } from './SessionContext';
+import { emptyProfile, Profile } from '../../lib/utils';
+
+export const ProfileContext = createContext<{ profile: Profile | null; setProfile: (profile: Profile) => void }>({
+  profile: null,
+  setProfile: () => undefined,
+});
+export function ProfileContextProvider({ children }: PropsWithChildren) {
+  const { setIsLoading } = useContext(LoadingContext);
+  const { session } = useContext(SessionContext);
+  const [profile, setProfile] = useState<Profile>(emptyProfile);
+
+  async function getProfile(session: Session | null) {
+    try {
+      setIsLoading(true);
+      if (!session?.user) throw new Error('No user on the session!');
+      const { error, status, data } = await supabase.from('profiles').select(`*`).eq('id', session?.user.id).single();
+      if (error && status !== 406) throw error;
+      if (error) return;
+      setProfile(data as Profile);
+    } catch (error) {
+      if (error instanceof Error) Alert.alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getProfile(session); // TODO: Test this
+  }, []);
+
+  return <ProfileContext.Provider value={{ profile, setProfile }}>{children}</ProfileContext.Provider>;
+}
