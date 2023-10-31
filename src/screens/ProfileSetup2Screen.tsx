cffbe95006a037291401301f65aaf046d28bb184
JonathanG321 <JonathanG321@ymail.com>
2023-10-31 13:26:07 -0400

    setup profileSetup2Screen for testing context

--- src/screens/ProfileSetup2Screen.tsx
+++ src/screens/ProfileSetup2Screen.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react';
-import { Text, View, Pressable } from 'react-native';
-import { Input } from 'react-native-elements';
+import { View } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../lib/utils';
+import { RootStackParamList } from '../../lib/types';
+import { useContext } from 'react';
+import { ProfileSetupContext } from '../contexts/ProfileSetupContext';
+import { Text } from 'react-native-elements';
 
 export type ProfileSetup2Props = NativeStackScreenProps<RootStackParamList, 'Profile Setup 2', 'Main'>;
 
 export default function ProfileSetup2Screen({ navigation: { navigate } }: ProfileSetup2Props) {
-  const [loading, setLoading] = useState(true);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const { profileSetup } = useContext(ProfileSetupContext);
+  console.log(profileSetup);
 
   return (
     <View className="p-3 mt-10">
-      <View className="py-1 self-stretch">
-        <Input label="First Name" value={firstName || ''} onChangeText={(text) => setFirstName(text)} />
-      </View>
-      <View className="py-1 self-stretch">
-        <Input label="Last Name" value={lastName || ''} onChangeText={(text) => setLastName(text)} />
-      </View>
-      <View className="mt-5 py-1 self-stretch">
-        <Pressable
-          className="flex items-center rounded bg-blue-500 active:bg-blue-600 px-2 py-1"
-          // onPress={() => updateProfile({ firstName, lastName })}
-          disabled={loading}
-        >
-          <Text className="text-white text-lg">{loading ? 'Loading ...' : 'Update'}</Text>
-        </Pressable>
-      </View>
+      <Text className="text-2xl mb-6 ml-6 text-[#5a5a5b]">Profile Setup 2</Text>
     </View>
   );
 }
