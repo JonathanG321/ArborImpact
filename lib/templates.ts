f4590a9fa7f07bbe951da3b33127b5e3cb91bb62
JonathanG321 <JonathanG321@ymail.com>
2023-11-01 14:37:14 -0400

    import sdgs into template

--- lib/templates.ts
+++ lib/templates.ts
@@ -7,5 +7,25 @@ export const emptyProfile = {
   wantDiversifyPortfolio: false,
   wantSpecificCause: false,
   wantTaxIncentives: false,
-  svg: [],
+  sdg: [],
+};
+
+export const SDGs = {
+  SDG1: require(`../assets/sdg/SDG1.png`),
+  SDG2: require(`../assets/sdg/SDG2.png`),
+  SDG3: require(`../assets/sdg/SDG3.png`),
+  SDG4: require(`../assets/sdg/SDG4.png`),
+  SDG5: require(`../assets/sdg/SDG5.png`),
+  SDG6: require(`../assets/sdg/SDG6.png`),
+  SDG7: require(`../assets/sdg/SDG7.png`),
+  SDG8: require(`../assets/sdg/SDG8.png`),
+  SDG9: require(`../assets/sdg/SDG9.png`),
+  SDG10: require(`../assets/sdg/SDG10.png`),
+  SDG11: require(`../assets/sdg/SDG11.png`),
+  SDG12: require(`../assets/sdg/SDG12.png`),
+  SDG13: require(`../assets/sdg/SDG13.png`),
+  SDG14: require(`../assets/sdg/SDG14.png`),
+  SDG15: require(`../assets/sdg/SDG15.png`),
+  SDG16: require(`../assets/sdg/SDG16.png`),
+  SDG17: require(`../assets/sdg/SDG17.png`),
 };
