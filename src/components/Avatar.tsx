7500751760c6c14bce5a25f8083d63fb1a0a1914
JonathanG321 <JonathanG321@ymail.com>
2023-11-03 13:36:49 -0400

    refactor Avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,26 +1,19 @@
-import { supabase } from '../../supabase/supabase';
 import { View, Alert, Image, Button } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 interface Props {
-  size: number;
   image: ImagePicker.ImagePickerAsset | null;
   onSelect: (file: ImagePicker.ImagePickerAsset) => void;
 }
 
 export default function Avatar({ image, onSelect }: Props) {
-  // useEffect(() => {
-  //   if (url) downloadImage(url);
-  // }, [url]);
-
   async function selectAvatar() {
     try {
       const { assets, canceled } = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
       });
 
       if (canceled || !assets.length) {
-        console.warn('cancelled');
         return;
       }
 
@@ -37,7 +30,7 @@ export default function Avatar({ image, onSelect }: Props) {
   }
 
   return (
-    <View className="flex flex-row justify-around items-center mb-3">
+    <View className="flex flex-row justify-around items-center mb-3 m-3">
       <View>
         <Button title="Choose File" onPress={selectAvatar} />
       </View>
@@ -45,10 +38,10 @@ export default function Avatar({ image, onSelect }: Props) {
         <Image
           source={image}
           accessibilityLabel="Avatar"
-          className={'rounded-md overflow-hidden max-w-full object-cover pt-0 w-24 h-24'}
+          className={'rounded-md overflow-hidden max-w-full object-cover pt-0 w-48 h-48'}
         />
       ) : (
-        <View className={'rounded-md overflow-hidden max-w-full bg-[#333] border border-[#bcbcbc] w-24 h-24'} />
+        <View className={'rounded-md overflow-hidden max-w-full bg-[#333] border border-[#bcbcbc] w-48 h-48'} />
       )}
     </View>
   );
