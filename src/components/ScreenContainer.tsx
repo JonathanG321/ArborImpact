5f1aa4a6d002e37be6ce5656a9c674ad9d19686c
JonathanG321 <JonathanG321@ymail.com>
2023-11-08 22:26:03 -0500

    fix screenContainer component

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -1,11 +1,17 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { PropsWithChildren } from 'react';
-import { View } from 'react-native';
+import { Dimensions, View } from 'react-native';
 
 export default function ScreenContainer({ children }: PropsWithChildren) {
+  const dimensions = Dimensions.get('window');
   return (
-    <LinearGradient className="h-full" colors={['#E5F0FF', '#fff']}>
-      <View className="p-3 mt-10 h-full">{children}</View>
-    </LinearGradient>
+    <View className="p-3 pt-10 h-full">
+      <LinearGradient
+        style={{ height: dimensions.height, width: dimensions.width }}
+        className="h-full z[-1] absolute top-0"
+        colors={['#E5F0FF', '#fff']}
+      />
+      {children}
+    </View>
   );
 }
