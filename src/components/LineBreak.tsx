999e7b5e48290f006c1a73a357fea0a5636a162c
JonathanG321 <JonathanG321@ymail.com>
2023-11-10 14:38:58 -0500

    refactor lineBreak

--- src/components/LineBreak.tsx
+++ src/components/LineBreak.tsx
@@ -2,7 +2,7 @@ import { View } from 'react-native';
 
 export default function LineBreak() {
   return (
-    <View className="px-2 mb-2">
+    <View className="px-2 mb-2 w-full">
       <View className="border-b-2 mx-2" />
     </View>
   );
