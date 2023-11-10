5b924163625743bf5f6b5e3673d9123239c32caf
JonathanG321 <JonathanG321@ymail.com>
2023-11-10 14:41:54 -0500

    build out ProjectScreen

--- src/screens/ProjectScreen.tsx
+++ src/screens/ProjectScreen.tsx
@@ -1,27 +1,65 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootDrawerParamList } from '../../lib/types';
-import { ProjectsContext } from '../contexts/ProjectsContext';
-import ProjectsCarousel from '../components/ProjectsCarousel';
-import { LinearGradient } from 'expo-linear-gradient';
-import { View } from 'react-native';
-import { Dimensions } from 'react-native';
+import { HeaderBackButton } from '@react-navigation/elements';
 import { useHeaderHeight } from '@react-navigation/elements';
 import { Text } from 'react-native-elements';
+import { RootDrawerParamList } from '../../lib/types';
 import ScreenContainer from '../components/ScreenContainer';
+import { View } from 'react-native';
+import LineBreak from '../components/LineBreak';
+import ButtonDisplay from '../components/ButtonDisplay';
 
 type Props = NativeStackScreenProps<RootDrawerParamList, 'Project'>;
 
 export default function ProjectScreen({
-  navigation: { navigate },
+  navigation: { navigate, setOptions },
   route: {
     params: { project },
   },
 }: Props) {
   const headerHeight = useHeaderHeight();
+  setOptions({ headerLeft: () => <HeaderBackButton onPress={() => navigate('Projects')} /> });
 
   return (
     <ScreenContainer>
-      <Text>{project.name}</Text>
+      <View className="flex items-center flex-1">
+        <Text className="text-2xl font-extrabold mb-4">{project.name.toLocaleUpperCase()}</Text>
+        <LineBreak />
+        <View className="w-full pl-4">
+          <Text className="text-2xl font-extrabold mb-4">
+            REGION: <Text className="font-normal text-lg">{project.region}</Text>
+          </Text>
+          <Text className="text-2xl font-extrabold mb-4">
+            IMPACT GOAL:{' '}
+            <Text className="font-normal text-lg">
+              {project.impactGoal} {project.impactGoalUnit}
+            </Text>
+          </Text>
+          <Text className="text-2xl font-extrabold mb-4">
+            ACTIVITY: <Text className="font-normal text-lg">{project.activity}</Text>
+          </Text>
+          <Text className="text-2xl font-extrabold mb-4">
+            TYPE OF IMPACT: <Text className="font-normal text-lg">{project.impactType}</Text>
+          </Text>
+        </View>
+        <LineBreak />
+        <View className="w-full flex flex-row my-6">
+          <ButtonDisplay text="VIEW SPENDING REPORTS" classNames="mx-4" textClassNames="font-bold" />
+        </View>
+        <Text className="text-xl font-extrabold mb-4">FUNDING GOAL AND PROGRESS</Text>
+        <LineBreak />
+        <View className="w-full pl-4">
+          <Text className="text-2xl text-blue-500 mb-1">
+            {project.donationCurrency} {project.donations.reduce((total, current) => total + current, 0).toFixed(2)}
+          </Text>
+          <Text className="text-md font-extrabold mb-4">
+            PLEDGED OF {project.donationCurrency} {project.fundingGoal.toLocaleString()}
+          </Text>
+          <Text className="text-xl font-extrabold mb-1">{project.donations.length}</Text>
+          <Text className="text-md font-extrabold mb-4">PROJECT FUNDERS</Text>
+          <Text className="text-xl font-extrabold mb-1">{}</Text>
+          <Text className="text-md font-extrabold mb-4">DAYS TO GO</Text>
+        </View>
+      </View>
     </ScreenContainer>
   );
 }
