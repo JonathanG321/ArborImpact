0b7095621609d67e27b4138d033f518ad028beb2
JonathanG321 <JonathanG321@ymail.com>
2023-11-09 21:01:11 -0500

    fix createProjectObject util to also deal with extra project images

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
 import { twMerge } from 'tailwind-merge';
 import { ClassValue, clsx } from 'clsx';
 import { supabase } from '../supabase/supabase';
-import { DBProject, Project } from './types';
+import { DBProject, Project, RootStackParamList } from './types';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -31,14 +32,40 @@ export async function downloadImage(path: string, bucket: string = 'avatars') {
 }
 
 export async function createProjectObject(dbProjects: DBProject[]) {
-  const projectImages = await Promise.all(
+  const projectMainImages = await Promise.all(
     dbProjects.map((project) => downloadImage(project.project_image_url, 'project_images'))
   );
+  const projectExtraImages = await Promise.all(
+    dbProjects.map(
+      async (project) => await Promise.all(project.extra_images.map((image) => downloadImage(image, 'project_images')))
+    )
+  );
   const projects = dbProjects.map(
-    ({ created_at, project_image_url, ...rest }, index) =>
+    (
+      {
+        created_at,
+        project_image_url,
+        extra_images,
+        funding_goal,
+        goal_date,
+        impact_goal,
+        impact_goal_unit,
+        impact_type,
+        ...rest
+      },
+      index
+    ) =>
       ({
         createdAt: new Date(created_at),
-        projectImage: projectImages[index] ? { uri: projectImages[index], width: 200, height: 200 } : null,
+        projectImage: projectMainImages[index] ? { uri: projectMainImages[index], width: 200, height: 200 } : null,
+        fundingGoal: funding_goal,
+        goalDate: goal_date,
+        impactGoal: impact_goal,
+        impactGoalUnit: impact_goal_unit,
+        impactType: impact_type,
+        extraImages: projectExtraImages[index]
+          ? projectExtraImages[index].map((image) => ({ uri: image, width: 200, height: 200 }))
+          : null,
         ...rest,
       } as Project)
   );
