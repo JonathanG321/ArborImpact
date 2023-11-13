21c4b3e7220a7663f66b055e754072e2c8b99f0f
JonathanG321 <JonathanG321@ymail.com>
2023-11-13 12:12:49 -0500

    refactor lineBreak component

--- src/components/LineBreak.tsx
+++ src/components/LineBreak.tsx
@@ -1,9 +1,6 @@
 import { View } from 'react-native';
+import { cn } from '../../lib/utils';
 
-export default function LineBreak() {
-  return (
-    <View className="px-2 mb-2 w-full">
-      <View className="border-b-2 mx-2" />
-    </View>
-  );
+export default function LineBreak({ classNames }: { classNames?: string }) {
+  return <View className={cn('px-2 mb-2 w-full border-b-2 mx-2', classNames)} />;
 }
