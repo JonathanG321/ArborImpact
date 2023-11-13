a5de91b17e347a366c3375ce37f78d09b4d14293
JonathanG321 <JonathanG321@ymail.com>
2023-11-13 12:13:14 -0500

    work on re building sign up screen

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,18 +1,24 @@
 import React, { useContext, useState } from 'react';
-import { Alert, View } from 'react-native';
+import { Alert, TextStyle, TouchableOpacity, View } from 'react-native';
 import { supabase } from '../../supabase/supabase';
-import { Input } from 'react-native-elements';
+import { Input, Text } from 'react-native-elements';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../lib/types';
 import { LoadingContext } from '../contexts/LoadingContext';
 import ScreenContainer from '../components/ScreenContainer';
 import AuthButton from '../components/AuthButton';
+import { useTailwind } from 'nativewind';
+import { cn } from '../../lib/utils';
+import LineBreak from '../components/LineBreak';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Sign Up', 'Main'>;
 
 export default function SignUpScreen({ navigation: { replace } }: Props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [disclaimer, setDisclaimer] = useState(false);
+  const [passError, setPassError] = useState<string | null>(null);
   const { setIsLoading } = useContext(LoadingContext);
 
   async function signUpWithEmail() {
@@ -30,10 +36,15 @@ export default function SignUpScreen({ navigation: { replace } }: Props) {
 
   return (
     <ScreenContainer>
+      <Text className="text-2xl text-center mb-2">Create your Arbor Account</Text>
+      <View className="flex flex-row justify-center">
+        <LineBreak classNames="w-1/2 border-gray-400" />
+      </View>
       <View className="mt-5 py-1 self-stretch">
         <Input
           label="Email"
-          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+          className="border pl-3"
+          labelStyle={useTailwind({ className: 'font-normal text-black text-xl mb-2' }) as TextStyle}
           onChangeText={setEmail}
           value={email}
           placeholder="email@address.com"
@@ -43,16 +54,34 @@ export default function SignUpScreen({ navigation: { replace } }: Props) {
       <View className="py-1 self-stretch">
         <Input
           label="Password"
-          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+          className={cn('border pl-3', { 'border-red-600': !!passError })}
+          labelStyle={useTailwind({ className: 'font-normal text-black text-xl mb-2' }) as TextStyle}
           onChangeText={setPassword}
           value={password}
           secureTextEntry={true}
-          placeholder="Password"
+          placeholder="password"
           autoCapitalize={'none'}
         />
       </View>
-      <AuthButton text="Sign Up" onClick={signUpWithEmail} />
-      <AuthButton text="Sign In Instead" onClick={() => replace('Sign In')} />
+      <View className="py-1 self-stretch">
+        <Input
+          label="Confirm Password"
+          className={cn('border pl-3', { 'border-red-600': !!passError })}
+          labelStyle={useTailwind({ className: 'font-normal text-black text-xl mb-2' }) as TextStyle}
+          onChangeText={setPasswordConfirm}
+          value={passwordConfirm}
+          secureTextEntry={true}
+          placeholder="password"
+          autoCapitalize={'none'}
+        />
+      </View>
+      <AuthButton text="Create Account" onClick={signUpWithEmail} />
+      <View className="flex flex-row justify-center">
+        <Text className="text-lg flex text-center mr-2">Already Have an Account?</Text>
+        <TouchableOpacity className="flex items-center" onPress={() => replace('Sign In')}>
+          <Text className="text-blue-400 text-lg">Sign In</Text>
+        </TouchableOpacity>
+      </View>
     </ScreenContainer>
   );
 }
