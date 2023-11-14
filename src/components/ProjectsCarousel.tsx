6e6b248d9eaa9204f75820472a88441088855e8c
JonathanG321 <JonathanG321@ymail.com>
2023-11-14 14:37:13 -0500

    use different carousel component

--- src/components/ProjectsCarousel.tsx
+++ src/components/ProjectsCarousel.tsx
@@ -1,45 +1,39 @@
 import { useRef } from 'react';
 import { Dimensions, ScaledSize, View } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
-import type { ICarouselInstance } from 'react-native-reanimated-carousel';
-import { useHeaderHeight } from '@react-navigation/elements';
-import { Project, RootDrawerParamList } from '../../lib/types';
 import { useNavigation } from '@react-navigation/native';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
+import Carousel from 'react-native-snap-carousel';
+import { Project, RootDrawerParamList } from '../../lib/types';
 import ProjectsCarouselItem from './ProjectsCarouselItem';
 
 const window: ScaledSize = Dimensions.get('window');
 
 const PAGE_WIDTH = window.width;
-const PAGE_HEIGHT = window.height;
 
-type Props = {
+type ProjectsProps = {
   projects: Project[];
 };
 
-export default function ProjectsCarousel({ projects }: Props) {
-  const ref = useRef<ICarouselInstance>(null);
-  const headerHeight = useHeaderHeight();
+export default function ProjectsCarousel({ projects }: ProjectsProps) {
+  const ref = useRef(null);
   const { navigate } = useNavigation<DrawerNavigationProp<RootDrawerParamList>>();
 
   return (
-    <View className="h-full">
+    <View>
       <Carousel
-        height={PAGE_HEIGHT - headerHeight}
-        width={PAGE_WIDTH}
-        style={{ height: '100%', width: '100%' }}
-        loop={false}
-        vertical
         ref={ref}
-        testID={'xxx'}
-        autoPlay={false}
-        autoPlayInterval={2000}
-        snapEnabled
-        pagingEnabled={false}
         data={projects}
-        mode="parallax"
-        modeConfig={{ parallaxScrollingOffset: 200 }}
-        renderItem={(props) => <ProjectsCarouselItem {...props} navigate={navigate} />}
+        inactiveSlideOpacity={1}
+        inactiveSlideScale={1}
+        fadingEdgeLength={50}
+        sliderWidth={PAGE_WIDTH}
+        slideStyle={{ height: 130, padding: 3 }}
+        firstItem={1}
+        itemWidth={130}
+        useScrollView={true}
+        renderItem={({ index, item }) => (
+          <ProjectsCarouselItem key={item.name} index={index} item={item} navigate={navigate} />
+        )}
       />
     </View>
   );
