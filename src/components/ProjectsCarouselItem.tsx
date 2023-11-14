27dd1b9399b651f07cc3b893c0a390c403443b2a
JonathanG321 <JonathanG321@ymail.com>
2023-11-14 14:38:56 -0500

    refactor ProjectCarouselItem to work with new carousel

--- src/components/ProjectsCarouselItem.tsx
+++ src/components/ProjectsCarouselItem.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import { LayoutRectangle, TouchableOpacity, View } from 'react-native';
 import { Image, Text } from 'react-native-elements';
-import { CarouselRenderItemInfo } from 'react-native-reanimated-carousel/lib/typescript/types';
 import { Project } from '../../lib/types';
 
-interface Props extends CarouselRenderItemInfo<Project> {
+type CarouselItem = {
+  item: Project;
+  index: number;
+};
+
+interface Props extends CarouselItem {
   navigate: (
     screen: 'Project',
     params: {
@@ -13,38 +17,34 @@ interface Props extends CarouselRenderItemInfo<Project> {
   ) => void;
 }
 
-export default function ProjectsCarouselItem({ animationValue, item, navigate }: Props) {
+export default function ProjectsCarouselItem({ item, navigate }: Props) {
   const [layout, setLayout] = useState<LayoutRectangle | null>(null);
   return (
     <View
       onLayout={(event) => {
         setLayout(event.nativeEvent.layout);
       }}
-      className="h-full justify-center items-center overflow-hidden"
+      className="h-full justify-center items-center overflow-hidden border border-gray-400"
     >
       {layout ? (
         <TouchableOpacity
           activeOpacity={0.6}
+          className="h-full w-full"
           onPress={() => {
-            if (animationValue.value === 0) {
-              navigate('Project', { project: item });
-            }
+            navigate('Project', { project: item });
           }}
         >
-          <View className="pb-3" style={{ height: layout.height * 0.15, width: layout.width }}>
-            <View className="bg-yellow-300 rounded-lg h-full w-full flex justify-center items-center">
-              <Text className="text-5xl font-extrabold">{item.name}</Text>
-            </View>
-          </View>
+          <View className="bg-gray-500 opacity-50 h-full w-full absolute bottom-0 left-0 z-[1]" />
+          <Text className="text-xl px-2 pb-1 text-white font-bold absolute bottom-0 left-0 z-[1]">{item.name}</Text>
           {!!item.projectImage ? (
             <Image
               source={{ ...item.projectImage }}
-              style={{ height: layout.height * 0.85, width: layout.width }}
+              style={{ height: layout.height, width: layout.width }}
               accessibilityLabel="Project Image"
-              className="rounded-md"
+              className="relative top-0 left-0"
             />
           ) : (
-            <View className="rounded-md max-w-full bg-[#333] border border-[#bcbcbc] w-48 h-48" />
+            <View className="max-w-full bg-[#333] border border-[#bcbcbc] w-48 h-48" />
           )}
         </TouchableOpacity>
       ) : null}
