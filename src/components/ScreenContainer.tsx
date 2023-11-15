81b1793836c118ecf85ebffeea8e3c873e4169ab
JonathanG321 <JonathanG321@ymail.com>
2023-11-14 22:29:40 -0500

    refactor ScreenContainer to use a ScrollView

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -1,17 +1,23 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { PropsWithChildren } from 'react';
-import { Dimensions, View } from 'react-native';
+import { Dimensions, ScrollView, View } from 'react-native';
 
-export default function ScreenContainer({ children }: PropsWithChildren) {
+interface Props extends PropsWithChildren {
+  scrollable?: boolean;
+}
+
+export default function ScreenContainer({ children, scrollable }: Props) {
   const dimensions = Dimensions.get('window');
   return (
-    <View className="p-3 pt-10 h-full">
+    <View className="h-full">
       <LinearGradient
         style={{ height: dimensions.height, width: dimensions.width }}
         className="h-full z[-1] absolute top-0"
         colors={['#E5F0FF', '#fff']}
       />
-      {children}
+      <ScrollView scrollEnabled={!!scrollable}>
+        <View className="p-3 pt-10">{children}</View>
+      </ScrollView>
     </View>
   );
 }
