a87871189ea69d10ee8f7443323ca2a79786bc0c
JonathanG321 <JonathanG321@ymail.com>
2023-11-17 16:03:40 -0500

    add NoProjectsPlaceholder Component

---
+++ src/components/NoProjectsPlaceholder.tsx
@@ -0,0 +1,23 @@
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+import Header from './Header';
+import ButtonDisplay from './ButtonDisplay';
+import { DrawerNavigationProp } from '@react-navigation/drawer';
+import { RootDrawerParamList } from '../../lib/types';
+
+type Props = {
+  navigate: DrawerNavigationProp<RootDrawerParamList, 'Profile', undefined>['navigate'];
+};
+
+export default function NoProjectsPlaceholder({ navigate }: Props) {
+  return (
+    <View className="flex mt-3 items-center">
+      <Header textClassNames="text-2xl" centered title="UH-OH!" />
+      <Text className="text-2xl mb-6 text-arbor-grey text-center">You don't have any projects yet!</Text>
+      <Text className="text-2xl mb-6 text-arbor-grey text-center">Click below to start browsing projects!</Text>
+      <View className="flex flex-row w-24">
+        <ButtonDisplay textClassNames="text-arbor-grey text-lg" text="Let's Go" onPress={() => navigate('Projects')} />
+      </View>
+    </View>
+  );
+}
