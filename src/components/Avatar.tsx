d67791ed5db4a2a03d354b332cea429de4f93eb4
JonathanG321 <JonathanG321@ymail.com>
2023-11-17 15:58:43 -0500

    genericize Avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,15 +5,16 @@ import { cn } from '../../lib/utils';
 interface Props {
   image?: ImagePicker.ImagePickerAsset | null;
   classNames?: string;
+  accessibilityLabel?: string;
 }
 
-function Avatar({ image, classNames }: Props) {
+function Avatar({ image, classNames, accessibilityLabel = 'Avatar' }: Props) {
   return (
     <>
       {!!image ? (
         <Image
           source={image}
-          accessibilityLabel="Avatar"
+          accessibilityLabel={accessibilityLabel}
           className={cn('rounded-md overflow-hidden max-w-full object-cover pt-0 w-48 h-48', classNames)}
         />
       ) : (
