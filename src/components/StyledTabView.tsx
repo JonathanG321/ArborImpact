bce0a9c266a8179a616889a8faf13962d7b1dddc
JonathanG321 <JonathanG321@ymail.com>
2023-11-21 19:33:55 -0500

    add StyledTabView component

---
+++ src/components/StyledTabView.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { View } from 'react-native';
+import { Route, SceneRendererProps, TabBar, TabBarIndicator, TabBarItem, TabView } from 'react-native-tab-view';
+
+type Props = {
+  defaultIndex?: number;
+  routes: Route[];
+  renderScene: (
+    props: SceneRendererProps & {
+      route: Route;
+    }
+  ) => React.ReactNode;
+};
+
+export default function StyledTabView({ renderScene, routes, defaultIndex = 0 }: Props) {
+  const [index, setIndex] = useState(defaultIndex);
+  return (
+    <View className="h-full px-4">
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            style={{ backgroundColor: 'transparent' }}
+            renderIndicator={(props) => (
+              <View style={{ height: props.layout.height, width: props.layout.width }}>
+                <TabBarIndicator {...props} style={{ backgroundColor: 'black', height: 3, marginBottom: 10 }} />
+              </View>
+            )}
+            renderTabBarItem={(props) => (
+              <TabBarItem
+                {...props}
+                labelStyle={{ color: 'black', fontWeight: '800', fontSize: 20, textAlign: 'center' }}
+              />
+            )}
+          />
+        )}
+      />
+    </View>
+  );
+}
