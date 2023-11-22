2b3f20d00c9798a00756463893923a480c642d8d
JonathanG321 <JonathanG321@ymail.com>
2023-11-22 16:05:38 -0500

    add disabled prop for ButtonDisplay

--- src/components/ButtonDisplay.tsx
+++ src/components/ButtonDisplay.tsx
@@ -8,12 +8,14 @@ type Props = {
   classNames?: string;
   text: string;
   onPress?: ((event: GestureResponderEvent) => void) | undefined;
+  disabled?: boolean;
 };
 
-export default function ButtonDisplay({ textClassNames, text, onPress, classNames }: Props) {
+export default function ButtonDisplay({ textClassNames, text, onPress, classNames, disabled = false }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={!!onPress ? 0.2 : 1}
+      disabled={disabled}
       className={cn('py-3 items-center flex-1 bg-arbor-yellow rounded-lg', classNames)}
       onPress={onPress}
     >
