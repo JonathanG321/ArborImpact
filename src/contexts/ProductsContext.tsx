849d220c9f769a99771a5fe55c80ffbc7b9bc630
JonathanG321 <JonathanG321@ymail.com>
2023-11-23 23:08:35 -0500

    add image processing to products context

--- src/contexts/ProductsContext.tsx
+++ src/contexts/ProductsContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, PropsWithChildren, useState } from 'react';
 import { Alert } from 'react-native';
-import { Product, ProjectWithDonations, SDG } from '../../lib/types';
-import { createProjectObjectsWithDonations, downloadImage } from '../../lib/utils';
+import { Product } from '../../lib/types';
+import { downloadImage } from '../../lib/utils';
 import Queries from '../../lib/supabaseQueries';
 
 export const ProductsContext = createContext<{
@@ -23,11 +23,14 @@ export function ProductsContextProvider({ children }: PropsWithChildren) {
       if (error) return;
       setProducts(
         await Promise.all(
-          data.map(async (product) => ({
-            ...product,
-            createdAt: product.created_at,
-            image: (await downloadImage(product.image_url || '')) || null,
-          }))
+          data.map(async (product) => {
+            const image = await downloadImage(product?.product_image_url || '', 'product_images');
+            return {
+              ...product,
+              createdAt: product.created_at,
+              image: image || null,
+            };
+          })
         )
       );
     } catch (error) {
